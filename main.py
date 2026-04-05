c8986bcd82cde81bafe1a7b213254efba1b76634
LALIT <LALIT.23FE10CDS00461@muj.manipal.edu>
2026-04-05 14:22:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import time
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader, UnstructuredMarkdownLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_community.vectorstores import FAISS
